599cfa01f7b157cdc2ba493d074933ad6cb42f2b
Donnie <donnie.ccj@gmail.com>
2018-11-15 11:59:12 +0100

    last commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+//import * as serviceWorker from './serviceWorker';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import CounterReducer from './CounterReducer';
+
+const store = createStore(
+CounterReducer,
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
